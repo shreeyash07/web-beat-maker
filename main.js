59561010363072822ec4fc6d4661381477c3ab32
Shreeyash Shrestha <shresthashree0807@gmail.com>
2021-01-29 00:03:50 +0545

    UI enhancement

--- main.js
+++ main.js
@@ -32,11 +32,18 @@ let songNotes = [];
 let isRecording = false;
 
 keys.forEach((key) => {
-  key.addEventListener("click", () => playNote(key));
+  key.addEventListener("click", () => {
+    playNote(key);
+  });
 });
 
 recordButton.addEventListener("click", () => {
   togglerRecording();
+});
+
+playButton.addEventListener("click", () => {
+  playSong();
+  //playButton.classList.add("active");
 });
 
 document.addEventListener("keydown", (e) => {
@@ -46,7 +53,9 @@ document.addEventListener("keydown", (e) => {
   const greenKeysIndex = GREEN_KEYS.indexOf(key);
   const blueKeysIndex = BLUE_KEYS.indexOf(key);
 
-  if (purpleKeysIndex > -1) playNote(purpleKeys[purpleKeysIndex]);
+  if (purpleKeysIndex > -1) {
+    playNote(purpleKeys[purpleKeysIndex]);
+  }
   if (greenKeysIndex > -1) playNote(greenKeys[greenKeysIndex]);
   if (blueKeysIndex > -1) playNote(blueKeys[blueKeysIndex]);
 });
@@ -63,10 +72,6 @@ function change() {
     startRecording();
     isRecording = true;
   }
-}
-
-function play() {
-  playSong();
 }
 
 function startRecording() {
@@ -90,6 +95,12 @@ function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
   noteAudio.currentTime = 0;
   noteAudio.play();
+
+  key.classList.add("active");
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
+
   if (isRecording) {
     recordNote(key.dataset.note);
   }
